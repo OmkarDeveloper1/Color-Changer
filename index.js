c1219ec7327ba43d47cdcf1e9f533badcf1b8da7
Omkar Lahare <148406882+OmkarDeveloper1@users.noreply.github.com>
2024-12-06 13:54:22 +0000

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let val2 = document.querySelector("#Body");
+let val = document.getElementById('text')
+
+function changeColor() {
+  val2.style.background = "cyan";
+}
+
+function changeYellow() {
+  val2.style.background = "red";
+}
+
+function changeviolet() {
+  val2.style.background = "blueviolet";
+}
+
+function changegold() {
+  val2.style.background = "gold";
+}
+
+function changeblack() {
+  val2.style.background = "black";
+}
+
+function textforBlack() {
+    val.style.color = 'white'
+  }
